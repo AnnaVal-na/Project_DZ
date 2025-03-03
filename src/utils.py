987ba441c62658f1dd43cfb718cbeec6ecfbaf09
Your Name <you@example.com>
2025-03-03 16:43:50 +0300

    init_logging_homework_12_1

--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,49 @@
 import json
+import logging
 import os
 from typing import Any, Dict, List
 
+# Проверка и создание директории для логов
+log_dir = 'logs'
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+# Настройка логирования для модуля utils
+logger = logging.getLogger('utils')
+logger.setLevel(logging.DEBUG)
+
+# Создание обработчика для записи логов в файл
+file_handler = logging.FileHandler(os.path.join(log_dir, 'utils.log'), mode='w')
+file_handler.setLevel(logging.DEBUG)
+
+# Форматирование логов
+file_formatter = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+
+# Добавление обработчика к логгеру
+logger.addHandler(file_handler)
+
 
 def load_transactions_from_json(file_path: str) -> List[Dict[str, Any]]:
-    """ Функция, которая принимает на вход путь до JSON-файла
-    и возвращает список словарей с данными о финансовых транзакциях.
-    Если файл пустой, содержит не список или не найден, функция возвращает
-    пустой список."""
-    # Проверяем, существует ли файл
+    """Функция для загрузки транзакций из JSON-файла."""
+    logger.debug(f"Попытка загрузить транзакции из файла: {file_path}")
+
     if not os.path.exists(file_path):
+        logger.error(f"Файл не найден: {file_path}")
         return []
 
-    # Пытаемся открыть и загрузить данные из JSON-файла
     try:
         with open(file_path, 'r') as file:
             data = json.load(file)
-            # Проверяем, является ли загруженные данные списком
             if isinstance(data, list):
+                logger.info(f"Успешно загружены {len(data)} транзакций из файла: {file_path}")
                 return data
             else:
+                logger.warning(f"Данные из файла не являются списком: {file_path}")
                 return []
-    except (json.JSONDecodeError, IOError):
+    except json.JSONDecodeError as e:
+        logger.error(f"Ошибка декодирования JSON в файле {file_path}: {e}")
+        return []
+    except IOError as e:
+        logger.error(f"Ошибка ввода-вывода при работе с файлом {file_path}: {e}")
         return []
